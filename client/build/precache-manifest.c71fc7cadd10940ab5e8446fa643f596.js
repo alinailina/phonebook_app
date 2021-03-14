c36a4819bd01e5d33664738ca18a2490701189dd
Alina Ilina <alinailina.random@gmail.com>
2021-03-14 16:51:44 +0200

    Deploy to Heroku

---
+++ client/build/precache-manifest.c71fc7cadd10940ab5e8446fa643f596.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f948938a847684a9970e3d2deaecb40b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64cfc014a6455076b79f",
+    "url": "/static/css/main.39dfeb3f.chunk.css"
+  },
+  {
+    "revision": "5a9c6189bd6f8f3c36cd",
+    "url": "/static/js/2.44024556.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.44024556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64cfc014a6455076b79f",
+    "url": "/static/js/main.346dc45d.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
